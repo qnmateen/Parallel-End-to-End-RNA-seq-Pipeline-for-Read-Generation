ee640c0012f896885fa4d76dfd7ecc7d22822410
Qazi Noorul Mateen <qnmateen@gmail.com>
2024-04-03 16:01:47 +0530

    Update setup_rnaseq_tools_latest.sh

--- setup_rnaseq_tools_latest.sh
+++ setup_rnaseq_tools_latest.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Define your conda environment name
-ENV_NAME="rnaseq-tools"
-
-# Create a new conda environment
-conda create -n $ENV_NAME -y
-
-# Activate the environment
-conda activate $ENV_NAME
-
 # Install bioinformatics tools with the latest versions
 conda install -c bioconda -y fastqc
 conda install -c bioconda -y trimmomatic
@@ -19,4 +10,4 @@ conda install -c bioconda -y sra-tools
 # Deactivate the environment
 conda deactivate
 
-echo "Installation completed. Use 'conda activate $ENV_NAME' to activate the environment."
+echo "Installation completed. Use 'conda activate env name to activate the environment."
